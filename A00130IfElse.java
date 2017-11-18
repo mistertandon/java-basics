d298016cfb4cf5bdbc35cbb9c8dd93de7ad27251
mistertandon <enggparveshtandon@gmail.com>
2017-11-18 17:34:06 +0530

    A00130IfElse: Developed code to implement IF-ELSE in java.

---
+++ A00130IfElse.java
@@ -0,0 +1,23 @@
+class A00130IfElse{
+	
+	public static void main(String[] args) {
+		
+		int numberA, numberB, numberC, biggestNumber;
+		
+		numberA = 160;
+		numberB = 100;
+		numberC = 300;
+		
+		if(numberA > numberB && numberA > numberC) {
+			
+			biggestNumber = numberA;
+		}else if(numberB > numberC) {
+			
+			biggestNumber = numberA;
+		}else {
+			
+			biggestNumber = numberC;
+		}
+		System.out.println("Biggest number is: "+biggestNumber);
+	}
+}
