59eaf1ed60e61f999dda9aa7eb236371b42d3c75
mistertandon <enggparveshtandon@gmail.com>
2017-11-18 16:38:36 +0530

    A00110PostIncrement: Developed java program to demonstrate post increment.

---
+++ A00110PostIncrement.java
@@ -0,0 +1,11 @@
+class A00110PostIncrement{
+	
+	public static void main(String a[]) {
+		
+		int numberA = 5;
+		
+		numberA++;
+		
+		System.out.printf("After Post increment value become: %d", numberA);
+	}
+}
