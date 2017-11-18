97ff0edaaa9aecd024031e7ec14aa83588dd8ad5
mistertandon <enggparveshtandon@gmail.com>
2017-11-18 16:43:22 +0530

    A00111PreIncrement.java: Developed java program to demonstrate Post increment.

---
+++ A00111PreIncrement.java
@@ -0,0 +1,11 @@
+class A00111PreIncrement{
+	
+	public static void main(String a[]) {
+		
+		int numberA = 5;
+		
+		++numberA;
+		
+		System.out.printf("After Pre increment value become: %d", numberA);
+	}
+}
