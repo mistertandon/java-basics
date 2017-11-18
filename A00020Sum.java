6f82603dd3ce8ef4b57c4a9ec8e9feba1cb10cfc
mistertandon <enggparveshtandon@gmail.com>
2017-11-18 07:45:57 +0530

    sum: Develop java program to add two numbers.

---
+++ A00020Sum.java
@@ -0,0 +1,13 @@
+class A00020Sum{
+	
+	public static void main(String a[]) {
+		
+		int addend = 5;
+		int augend = 8;
+		int sum = 0;
+		
+		sum = addend+augend;
+		
+		System.out.print(sum);
+	}
+}
