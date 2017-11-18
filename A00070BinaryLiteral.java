84ed4e26c10acfe8fe58b0d9847c0c6bf595bd9c
mistertandon <enggparveshtandon@gmail.com>
2017-11-18 11:54:34 +0530

    A00070BinaryLiteral: Developed java program to demonstrate binary literal in java.

---
+++ A00070BinaryLiteral.java
@@ -0,0 +1,11 @@
+class A00070BinaryLiteral{
+	
+	public static void main(String a[]) {
+		
+		int rawNumber;
+		
+		rawNumber = 0B1_00_00_00_00;
+		
+		System.out.println("Binary to decimal is : "+rawNumber);
+	}
+}
