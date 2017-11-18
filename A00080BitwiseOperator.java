85e18615f19e2f247b96730a2f3cf0ee3a65569f
mistertandon <enggparveshtandon@gmail.com>
2017-11-18 16:22:57 +0530

    A00080BitwiseOperator: Developed java program to demonstrate Bitwise AND & OR operator

---
+++ A00080BitwiseOperator.java
@@ -0,0 +1,17 @@
+class A00080BitwiseOperator{
+	
+	public static void main(String a[]) {
+		
+		int numberA = 25;
+		int numberB = 15;
+		int result = 0;
+		
+		result = numberA & numberB;
+		System.out.println("Bitwise AND operator functionality.");
+		System.out.printf("Result after bitwise & opeartion on %d and %d is : %d ", numberA, numberB, result);
+		
+		result = numberA | numberB;
+		System.out.println("Bitwise OR operator functionality.");
+		System.out.printf("Result after bitwise | opeartion on %d and %d is : %d ", numberA, numberB, result);
+	}
+}
