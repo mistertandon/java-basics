d6e6e96c935277cc06e768348efc70dc93eafcb4
mistertandon <enggparveshtandon@gmail.com>
2017-11-18 16:24:03 +0530

    A00090LeftShiftOperator: Developed java code demonstrate left shift operator.

---
+++ A00090LeftShiftOperator.java
@@ -0,0 +1,16 @@
+class A00090LeftShiftOperator{
+	
+	public static void main(String a[]) {
+		
+		int numberA = 25;
+		int result;
+		
+		result = numberA << 3;
+		/*	
+		 * 	numberA << 3 i.e. binary representation of numberA will shift by 2 bits after adding two bits on right most side.
+		 * 	numberA 11001 will convert to 11001000
+		 * 
+		 */
+		System.out.printf("After apply left shift operator on numberA i.e %d, it will become %d ", numberA, result);
+	}
+}
