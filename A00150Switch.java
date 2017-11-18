09a4ef5a599b39b536d445ccb9173f8039a7de4f
mistertandon <enggparveshtandon@gmail.com>
2017-11-18 18:11:35 +0530

    A00150Switch: Developed java program to implement switch case.

---
+++ A00150Switch.java
@@ -0,0 +1,21 @@
+class A00150Switch{
+	
+	public static void main(String[] args) {
+		
+		int passedArgument;
+		
+		switch(args[0]) {
+			
+		case "1": passedArgument = 1;
+						break;
+		case "2": passedArgument = 2;
+						break;
+		case "3": passedArgument = 3;
+						break;
+		default: passedArgument = 10;
+						break;
+		}
+		
+		System.out.print("Passed argument is: "+passedArgument);
+	}
+}
