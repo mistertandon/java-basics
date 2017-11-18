f1d89c0858973f96ccd04c77464bcfb0a69f3075
mistertandon <enggparveshtandon@gmail.com>
2017-11-18 17:39:25 +0530

    A00140Ternary: Developed java program to demonstrate TERNARY operator.

---
+++ A00140Ternary.java
@@ -0,0 +1,13 @@
+class A00140Ternary{
+	
+	public static void main(String[] args) {
+		
+		int rateofinterest = 6, finalinterest;
+		Boolean isExecution = true;
+		
+		
+		finalinterest = isExecution?rateofinterest:8;
+		
+		System.out.println("Rate of interest is : "+finalinterest);
+	}
+}
