56dd4ae0470fe1679718d1cf5c112b7f4887b5f2
mistertandon <enggparveshtandon@gmail.com>
2017-11-18 11:53:35 +0530

    A00060ShorthandOperator: Developed java program to show shorthand operator usage.

---
+++ A00060ShorthandOperator.java
@@ -0,0 +1,15 @@
+class A00060ShorthandOperator{
+	
+	public static void main(String a[]) {
+		
+		byte operand = 5;
+		
+		operand *= 2;
+		System.out.println("Multiplication shorthand result (Multiplicand and Multiplier are of both data type) : "+operand);
+		
+		operand = 5;
+		operand *= 4.5;
+		System.out.println("Note: Data type of left operand dominates when Left side operand and Right side operand are of different data type");
+		System.out.println("Multiplication shorthand result (Multiplicand and Multiplier are of different data type): "+operand);
+	}
+}
