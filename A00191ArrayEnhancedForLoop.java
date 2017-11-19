c8e3bca1906642eb99c01b27c22b87e098282bfe
mistertandon <enggparveshtandon@gmail.com>
2017-11-19 12:28:24 +0530

    A00191ArrayEnhancedForLoop: Developed java program to demonstrate Enhanced For Loop for Array.

--- A00191ArrayEnhancedForLoop.java
+++ A00191ArrayEnhancedForLoop.java
@@ -4,10 +4,8 @@ public class A00191ArrayEnhancedForLoop {
 	public static void main(String[] args) {
 
 		int arrayCont[] = { 5, 67, 82, 109, 43 };
-
 		
 		for (int elemVal : arrayCont) {
-
 
 			System.out.println("element value is: " + elemVal);
 		}
