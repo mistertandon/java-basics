59487505b84425b75e8bad59edc7d13c8da594f1
mistertandon <enggparveshtandon@gmail.com>
2017-11-19 05:31:16 +0530

    A00181PatternII: Developed java program to implement pattern.

---
+++ A00181PatternII.java
@@ -0,0 +1,21 @@
+
+public class A00181PatternII {
+
+	public static void main(String[] args) {
+
+		int outerLoopLimit, innerLoopLimit;
+
+		outerLoopLimit = innerLoopLimit = 5;
+
+		for (int outerLoopInit = 1; outerLoopInit <= outerLoopLimit; outerLoopInit++) {
+
+			for (int innerLoopInit = 1; innerLoopInit <= outerLoopInit; innerLoopInit++) {
+
+				System.out.print("* ");
+			}
+
+			System.out.println("");
+
+		}
+	}
+}
