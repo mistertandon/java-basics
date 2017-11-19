e53c18ad1c2a96dd1f33a950d2aef22b01722189
mistertandon <enggparveshtandon@gmail.com>
2017-11-19 19:48:27 +0530

    A00193Array2D: Developed code to implement 2-D Array.

---
+++ A00193Array2D.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class A00193Array2D {
+
+	public static void main(String[] args) {
+		
+		int rowsCount = 3, columnsCount = 4;
+		int arrayCont[][] = new int[rowsCount][columnsCount];
+		Random randomObj = new Random();
+		
+		for (int rowsInit = 0; rowsInit < rowsCount; rowsInit++) {
+			
+			for (int coulmnsInit = 0; coulmnsInit < columnsCount; coulmnsInit++) {
+					
+				arrayCont[rowsInit][coulmnsInit] = randomObj.nextInt(300);
+
+				System.out.println(
+						"Array [" + rowsInit + "][" + coulmnsInit + "] the index value is: " + arrayCont[rowsInit][coulmnsInit]);
+			}
+		}
+
+	}
+
+}
