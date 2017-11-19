7f4046253aa39e2ae8989e0bacd4eecca2405b3e
mistertandon <enggparveshtandon@gmail.com>
2017-11-19 18:27:03 +0530

    A00192ArrayRandom: Developed java program to demonstrate array using Random class object to initialize array element.

---
+++ A00192ArrayRandom.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class A00192ArrayRandom {
+
+	public static void main(String[] args) {
+
+		int arrayCont[] = new int[5];
+		Random randomObj = new Random();
+
+		for (int iterativeInit = 0; iterativeInit < arrayCont.length; iterativeInit++) {
+
+			arrayCont[iterativeInit] = randomObj.nextInt(200);
+		}
+
+		for (int elemVal : arrayCont) {
+
+			System.out.println("element value is: " + elemVal);
+		}
+	}
+}
