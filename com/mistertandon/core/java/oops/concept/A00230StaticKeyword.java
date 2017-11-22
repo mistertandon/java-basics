84812f74722355d4e49419a17d5519475ec85a7d
mistertandon <enggparveshtandon@gmail.com>
2017-11-22 21:35:39 +0530

    A00230StaticKeyword: Developed java program to demonstrate STATIC function.

---
+++ com/mistertandon/core/java/oops/concept/A00230StaticKeyword.java
@@ -0,0 +1,18 @@
+package com.mistertandon.core.java.oops.concept;
+
+public class A00230StaticKeyword {
+
+	public static void main(String[] args) {
+
+		PrinterMachine.print("That is how we can call static function of a class.");
+	}
+
+}
+
+class PrinterMachine {
+
+	public static void print(String text) {
+
+		System.out.println("Output of the string : " + text);
+	}
+}
