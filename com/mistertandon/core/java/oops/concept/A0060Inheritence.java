d54ecfe466aeb7bec606d41967efe99efc78be79
mistertandon <enggparveshtandon@gmail.com>
2017-11-23 18:35:34 +0530

    A0060Inheritence: Modified file to remove unneccessary class properties.

--- com/mistertandon/core/java/oops/concept/A0060Inheritence.java
+++ com/mistertandon/core/java/oops/concept/A0060Inheritence.java
@@ -3,78 +3,71 @@ package com.mistertandon.core.java.oops.concept;
 public class A0060Inheritence {
 
 	public static void main(String[] args) {
-		
+
 		Multiplication multiplicationObj = new Multiplication();
-		
+
 		multiplicationObj.setAugend(4);
 		multiplicationObj.setAddend(45);
 		multiplicationObj.getSum();
 		multiplicationObj.displaySum();
-		
+
 		multiplicationObj.setMultiplicand(2);
 		multiplicationObj.setMultiplier(23);
 		multiplicationObj.getMultiplication();
 		multiplicationObj.displayMultiplication();
-		
+
 	}
 }
 
 class SumII {
 
-	private int augend;
+	public int operandA;
 
-	private int addend;
+	public int operandB;
 
-	private int addedValue;
+	public int result;
 
 	public void setAugend(int augend) {
 
-		this.augend = augend;
+		operandA = augend;
 	}
 
 	public void setAddend(int addend) {
 
-		this.addend = addend;
+		operandB = addend;
 	}
 
 	public void getSum() {
 
-		addedValue = augend + addend;
+		result = operandA + operandB;
 	}
 
 	public void displaySum() {
 
-		System.out.printf("Sum of AUGEND %d and ADDEND %d is: %d \n", augend, addend, addedValue);
+		System.out.printf("Sum of AUGEND %d and ADDEND %d is: %d \n", operandA, operandB, result);
 	}
 
 }
 
-class Multiplication extends SumII{
-
-	private int multiplicand;
-
-	private int multiplier;
-
-	private int multiplicationValue;
+class Multiplication extends SumII {
 
 	public void setMultiplicand(int multiplicand) {
 
-		this.multiplicand = multiplicand;
+		operandA = multiplicand;
 	}
 
 	public void setMultiplier(int multiplier) {
 
-		this.multiplier = multiplier;
+		operandB = multiplier;
 	}
 
 	public void getMultiplication() {
 
-		this.multiplicationValue = multiplicand * multiplier;
+		result = operandA * operandB;
 	}
 
 	public void displayMultiplication() {
 
-		System.out.printf("Multiplication of Multiplier %d and Multiplicand %d is: %d\n", multiplicand, multiplier,
-				multiplicationValue);
+		System.out.printf("Multiplication of Multiplier %d and Multiplicand %d is: %d\n", operandA, operandB, result);
 	}
 }
