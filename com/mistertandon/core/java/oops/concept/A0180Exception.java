a4ac7e8d6a438a2a4ccd5f4ad2c59b3ddeca8310
mistertandon <enggparveshtandon@gmail.com>
2017-11-25 22:48:11 +0530

    A0180Exception: Developed java program to demonstrate Exception handling.

---
+++ com/mistertandon/core/java/oops/concept/A0180Exception.java
@@ -0,0 +1,69 @@
+package com.mistertandon.core.java.oops.concept;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class A0180Exception {
+
+	public static void main(String[] args) {
+
+		int dividend;
+		int divisor;
+		int quotient;
+
+		try {
+
+			BufferedReader bufferedReaderObj = new BufferedReader(new InputStreamReader(System.in));
+
+			dividend = Integer.parseInt(bufferedReaderObj.readLine());
+			divisor = Integer.parseInt(bufferedReaderObj.readLine());
+
+			Divide divideObj = new Divide(dividend, divisor);
+
+			divideObj.makeDivision();
+			divideObj.displayQuotient();
+
+		} catch (ArithmeticException e) {
+
+			System.out.println("Arithmetic Eception occur.");
+		} catch (Exception e) {
+
+			System.out.println("General Eception occur.");
+		}
+
+	}
+}
+
+class Divide {
+
+	private int dividend;
+
+	private int divisor;
+
+	private int quotient;
+
+	public Divide(int dividend, int divisor) {
+
+		this.dividend = dividend;
+		this.divisor = divisor;
+	}
+
+	public void makeDivision() {
+
+		try {
+
+			quotient = dividend / divisor;
+		} catch (ArithmeticException e) {
+
+			System.out.println("Arithmetic Eception occur.");
+		} catch (Exception e) {
+
+			System.out.println("General Eception occur.");
+		}
+	}
+
+	public void displayQuotient() {
+
+		System.out.printf("Quotient is : %d", quotient);
+	}
+}
